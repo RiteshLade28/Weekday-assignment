de31b912a12dfc11dc26bbb1738f6399f514d8c1
RiteshLade28 <riteshlade10@gmail.com>
2024-05-05 15:33:24 +0530

    card completed withour blur effect

--- src/components/JobCard/JobCard.js
+++ src/components/JobCard/JobCard.js
@@ -5,39 +5,76 @@ import { red } from "@mui/material/colors";
 import Box from "@mui/material/Box";
 import "./JobCard.scss";
 import Image from "next/image";
+import HourglassTopIcon from "@mui/icons-material/HourglassTop";
+import { Button } from "@mui/material";
 
 export default function JobCard() {
   return (
     <Box className="jobCard">
       <Box className="cardHeader">
-        <Box className="cardLogo">
-          <Image
-            src="/fampayLogo.jpg"
-            alt="FamPay Logo"
-            width={50}
-            height={50}
-          />
-        </Box>
-        <Box className="cardDetails">
-          <Typography variant="subtitle1" component="div">
-            fampay
-          </Typography>
-          <Typography variant="h6" component="div">
-            Backend Engineer
-          </Typography>
+        <Box className="cardCreated">
+          <HourglassTopIcon fontSize="small" style={{ color: "orange" }} />
           <Typography variant="body2" component="div">
-            Location
+            Posted 10 days ago
           </Typography>
+        </Box>
+        <Box className="cardInfo">
+          <Box className="cardLogo">
+            <Image
+              src="/fampayLogo.jpg"
+              alt="FamPay Logo"
+              width={50}
+              height={50}
+            />
+          </Box>
+          <Box className="cardDetails">
+            <Typography
+              fontWeight="bold"
+              color="#504e4e"
+              variant="subtitle1"
+              component="div"
+            >
+              fampay
+            </Typography>
+            <Typography variant="h6" component="div">
+              Backend Engineer
+            </Typography>
+            <Typography
+              style={{
+                fontWeight: "bold",
+                color: "#504e4e",
+              }}
+              variant="body2"
+              component="div"
+            >
+              Location
+            </Typography>
+          </Box>
         </Box>
       </Box>
       <Box className="cardContent">
-        <Typography variant="body2" component="div">
-          Estimated Salary
+        <Typography
+          style={{ fontWeight: "550", color: "#504e4e" }}
+          variant="subtitle1"
+          component="div"
+        >
+          Estimated Salary: ₹18 - 35 LPA ✅
         </Typography>
-        <Typography variant="body2" component="div">
+        <Typography
+          style={{ fontWeight: "550", color: "#504e4d" }}
+          variant="h6"
+          component="div"
+        >
           About Company
         </Typography>
-        <Typography variant="body2" component="div">
+        <Typography variant="body1" component="div">
+          <Typography
+            style={{ fontWeight: "600", color: "#504e4d" }}
+            variant="body1"
+            component="div"
+          >
+            About Us
+          </Typography>
           FamPay is building India’s first neo-bank exclusively teens. FamPay
           helps teens make their own online and offline payments through UPI,
           FamPay App and FamCard. Our aim is to make banking cool for teens and
@@ -50,6 +87,52 @@ export default function JobCard() {
           India, Venture Highway, Global Founder’s Capital and the likes of
           Kunal Shah, Amrish Rao as angels.
         </Typography>
+        <Box style={{ display: "flex", justifyContent: "center" }}>
+          <button
+            style={{
+              backgroundColor: "transparent",
+              border: "none",
+              cursor: "pointer",
+              fontSize: "1rem",
+              color: "blue",
+              marginTop: "10px"
+            }}
+          >
+            View More
+          </button>
+        </Box>
+      </Box>
+      <Box style={{ marginTop: "10px" }}>
+        <Typography
+          style={{ fontWeight: "600", color: "#504e4d" }}
+          variant="body1"
+          component="div"
+        >
+          Minimum Experience
+        </Typography>
+        <Typography variant="body1" component="div">
+          2 year
+        </Typography>
+        <Box className="cardFooter">
+          <Button
+            style={{
+              backgroundColor: "#55EFC4",
+              color: "black",
+              fontWeight: "bold",
+            }}
+            variant="contained"
+            color="primary"
+          >
+            ⚡ Easy Apply
+          </Button>
+          <Button
+            style={{ backgroundColor: "#10439F" }}
+            variant="contained"
+            color="primary"
+          >
+            Unlock Referrel Ask
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
